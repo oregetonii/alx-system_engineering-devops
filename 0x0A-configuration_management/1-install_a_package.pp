4f3e098541b4c39c32183eb1f9e1667eea67bbd0
root <root@DESKTOP-6B7A13C.localdomain>
2024-03-22 16:15:14 +0300

    correction4 to package installer

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
 #Installs flask package from pip3
+exec { 'apt-get update':
+command => '/usr/bin/apt-get update',
+}
+
+package { 'python3':
+ensure => 'installed',
+}
+
+package { 'python3-pip':
+ensure  => 'installed',
+require => Package['python3'],
+}
+
 package { 'flask':
 ensure   => '2.1.0',
 provider => 'pip',
-require  => Class['python'],
+require  => Package['python3'],
 }
