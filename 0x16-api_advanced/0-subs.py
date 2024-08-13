aaf9257a4453e194de7093019353c9d73f384a78
oregetonii <proffzion30@gmail.com>
2024-08-13 20:08:11 +0300

    fix pycodestyle on 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
-Function that queries the Rddit API and returns the number of subscribers for given subreddit
+Function that queris the Reddit API and return number of subscribers.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API and returns the number of subscribers for a given subreddit
+    Function that queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome.58.0.3029.110 Safari/537.3)'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64'
+               'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110'
+               'Safari/537.3)'}
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False, timeout=20)
-        # If the response status code is 302, it indicates a redirect, possibly to search results
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        # If the response status code is 302, it indicates a redirect
         if response.status_code == 302:
             return 0
         # If the response status code is not 200, something went wrong
